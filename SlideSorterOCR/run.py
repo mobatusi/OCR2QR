91630ede9273365b63df6b55d53c7023457effa9
Juan Carlos Vizcarra <jcvspem@gmail.com>
2017-07-18 16:34:43 -0400

    Modified two files

--- SlideSorterOCR/run.py
+++ SlideSorterOCR/run.py
@@ -1,4 +1,4 @@
-#!flask/bin/python2
+#!flask/bin/python
 from flask import Flask
 from flask_cors import CORS, cross_origin
 from flask import Response
@@ -6,7 +6,7 @@ import json
 from flask import request
 import urllib
 import imghdr
-from PIL import Image  ###
+from PIL import Image
 from pytesseract import *
 import os.path
 # from pytesseract import *
@@ -26,7 +26,6 @@ def index():
     #this converts user into a json object
 
 @app.route('/getImage')
-# @crossdomain(origin='*')
 def getImage():
 	user = {'imageURL': request.args.get('imageURL'), 'imageFlag': False, 'sampleOutput': False}
 	# urllib.urlretrieve(user['imageURL'], "sample.png")
@@ -34,12 +33,13 @@ def getImage():
 	
 	#check if file is a png image
 	if imghdr.what('sample.png') == 'png': #if true then run ocr
+		# print "hello"
 		user['imageFlag'] = os.path.isfile('sample.png')
 		# user['imageURL'] = True
 		im = Image.open('sample.png')
 
 		text = image_to_string(Image.open('sample.png'))
-		user['sampleOutput'] = text
+	# 	user['sampleOutput'] = text
 		# cors = CORS(im)
 		# CORS(app)
 		# CORS(im)
