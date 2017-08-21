e2c1ea23652d9396e22894543f2ca3dd5fe42bf0
Juan Carlos Vizcarra <jcvspem@gmail.com>
2017-08-21 11:32:43 -0400

    Added regex capabilities to the app. This makes it able to run regex on the currently displayed ocr output. The results are placed on the individual group boxes and the user can decide whether to update these results to the API or not.

--- SlideSorterOCR/run.py
+++ SlideSorterOCR/run.py
@@ -27,7 +27,6 @@ def index():
 
 @app.route('/getImage')
 def getImage():
-	print "helooooooooooooooooooooooooooooooooo"
 	#*urls with & have problems, seem to cut off the passed parameter url at the first occurance of &
 	user = {'sampleOutput': ''}
 	thresh = 95 #the threshold for the black and white conversion
@@ -77,7 +76,7 @@ def getMetadata():
 	API_URL = "http://candygram.neurology.emory.edu:8080/api/v1"
 	#sign-in to the client using user & password
 	gc = girder_client.GirderClient(apiUrl=API_URL)
-	gc.authenticate(username="add the administrative username", password="")
+	gc.authenticate(username="", password="")
 	temp = request.args.get('imageID')
 	# print 'The current image id is ',temp
 	temp = "item/" + temp
@@ -96,7 +95,7 @@ def updateMetadata():
 	API_URL = "http://candygram.neurology.emory.edu:8080/api/v1"
 	#sign-in to the client using user & password
 	gc = girder_client.GirderClient(apiUrl=API_URL)
-	gc.authenticate(username="add the administrative username", password="")
+	gc.authenticate(username="", password="")
 	temp = request.args.get('imageID')
 	# temp = "item/" + temp + "/metadata"
 	# print temp
@@ -119,7 +118,7 @@ def updateMetadataGroup():
 	API_URL = "http://candygram.neurology.emory.edu:8080/api/v1"
 	# #sign-in to the client using user & password
 	gc = girder_client.GirderClient(apiUrl=API_URL)
-	gc.authenticate(username="add admininstrative username", password="")
+	gc.authenticate(username="", password="")
 	groups = gc.getItem(imageID)['meta']
 	if groups.has_key('groups'):
 		groups = groups['groups']
@@ -132,6 +131,36 @@ def updateMetadataGroup():
 		return "Metadata was saved!"
 	else:
 		return "This slide has no groups meta data currently"
+
+def cleanFoxMetaDataOutput(dirtyOutput):
+	if dirtyOutput['BrainID'] != None:
+		dirtyOutput['BrainID'] = re.sub('\s','',dirtyOutput['BrainID']).upper() #remove any spaces from brain ID
+		dirtyOutput['BrainID'] = re.sub('FOX','Fox',dirtyOutput['BrainID'])    
+	if dirtyOutput['Hemi'] != None:
+		dirtyOutput['Hemi'] = re.sub('(?i)LH','LH',dirtyOutput['Hemi'])
+		dirtyOutput['Hemi'] = re.sub('(?i)Cau','Caudal',dirtyOutput['Hemi'])
+		dirtyOutput['Hemi'] = re.sub('(?i)LH M','LH Mid',dirtyOutput['Hemi'])
+		dirtyOutput['Hemi'] = re.sub('(?i)(LH F|LHF)','LH Frontal',dirtyOutput['Hemi'])
+	dirtyOutput['Stain'] = re.sub('(?i)(AVP)','AVP 1:20k',dirtyOutput['Stain'])
+	dirtyOutput['Stain'] = re.sub('(?i)(ss|lesl)','Nissl',dirtyOutput['Stain'])
+	dirtyOutput['Stain'] = re.sub('(?i)(Slver|Silver)','Silver',dirtyOutput['Stain'])
+	dirtyOutput['StainDate'] = re.sub('\/{1,}','/',dirtyOutput['StainDate'])
+	dirtyOutput['StainDate'] = re.sub('\s','',dirtyOutput['StainDate'])
+	return dirtyOutput
+
+
+@app.route('/runRegex')
+def runRegex():
+	noRegexMatch = {'groups':{'BrainID':None,'Hemi':None,'Column':None,'Section':None,'Stain':None,'StainDate':None}}
+	sampleRegex = '(?P<BrainID>Fox ?\d ?\d?( )?(?i)(u|t|a))? ?(?P<Hemi>(?i)(LH M|LH F|LH Cau|LHF|Cau))?.* ?(?i)(Cd|Co|Col) ?(?P<Column>(0|1|2|3|4|5|6|7|9)) (?i)(3|8|s) ?(?P<Section>\d{1,3}b?).*(?P<Stain>(?i)(ss|Slver|Silver|lesl|AVP)).* (?P<StainDate>(\d\d?\/\d\d?\/1(6|7)|\d\d?\/\/?1(6|7)))'
+	re_ADRC_SlideSet = re.compile(sampleRegex)
+	m = re_ADRC_SlideSet.search(request.args.get('ocr_output'))
+
+	if m:
+		noRegexMatch['groups'] = m.groupdict()
+		noRegexMatch['groups'] = cleanFoxMetaDataOutput(noRegexMatch['groups']) 
+
+	return Response(json.dumps(noRegexMatch))
 
 
 app.run(debug=True)
